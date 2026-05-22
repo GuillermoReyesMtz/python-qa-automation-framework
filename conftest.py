2f157e719e57103d9fecf87413f3611988c5082c
Guillermo Reyes Martinez <guillermo.reyes@argano.com>
2026-05-22 17:57:24 -0600

    Configure headless Chrome for GitHub Actions

--- conftest.py
+++ conftest.py
@@ -2,7 +2,7 @@ import os
 import pytest
 
 from datetime import datetime
-
+from selenium.webdriver.chrome.options import Options
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
@@ -11,8 +11,15 @@ from webdriver_manager.chrome import ChromeDriverManager
 @pytest.fixture
 def driver():
 
+    chrome_options = Options()
+
+    chrome_options.add_argument("--headless")
+    chrome_options.add_argument("--no-sandbox")
+    chrome_options.add_argument("--disable-dev-shm-usage")
+
     driver = webdriver.Chrome(
         service=Service(ChromeDriverManager().install())
+        options=chrome_options
     )
 
     driver.maximize_window()
