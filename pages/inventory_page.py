229d538493dee0e0b3ccf7bd55789212be5503e8
Guillermo Reyes Martinez <guillermo.reyes@argano.com>
2026-05-22 15:49:55 -0600

    Implement logout flow with explicit waits and inventory page object

---
+++ pages/inventory_page.py
@@ -0,0 +1,26 @@
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
+class InventoryPage:
+
+    BURGER_MENU_BUTTON = (By.ID, "react-burger-menu-btn")
+    LOGOUT_LINK = (By.ID, "logout_sidebar_link")
+
+    def __init__(self, driver):
+        self.driver = driver
+    
+    def click_burger_menu(self):
+        self.driver.find_element(*self.BURGER_MENU_BUTTON).click()
+
+    def click_logout(self):
+
+        logout_button = WebDriverWait(self.driver, 10).until(
+            EC.element_to_be_clickable(self.LOGOUT_LINK)
+        )
+
+        logout_button.click()
+    
+    def logout(self):
+        self.click_burger_menu()
+        self.click_logout()
